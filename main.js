e27a3b2a210ea7621894b864bb9628dfc39dea2b
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2022-08-21 15:58:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function BedRoom() {
+    window.location = "bedroom.html";
+}
+
+function Back() {
+    window.location = "index.html";
+}
+
+function TVAC() {
+    window.location = "TV and AC.html";
+}
+
+function Desk() {
+    window.location = "Desk.html";
+}
+
+function Bottles() {
+    window.location = "bottles.html";
+}
+
+function fruitbasket() {
+    window.location = "fruitbasket.html";
+}
